30bdd46e8fe23a2654d86062eecf709881e11ce0
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-07 11:08:15 +0900

    add LED
    밝기에 따라 LED ON 개수 변경

--- switchCase_apply.ino
+++ switchCase_apply.ino
@@ -1,9 +1,16 @@
 const int sensorMin = 0;    // sensor minimum, discovered through experiment
 const int sensorMax = 600;  // sensor maximum, discovered through experiment
 
+const int LED1 = 6;
+const int LED2 = 9;
+const int LED3 = 10;
+
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
 }
 
 void loop() {
@@ -16,15 +23,27 @@ void loop() {
   switch (range) {
     case 0:  // your hand is on the sensor
       Serial.println("dark");
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, HIGH);
       break;
     case 1:  // your hand is close to the sensor
       Serial.println("dim");
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED3, LOW);
       break;
     case 2:  // your hand is a few inches from the sensor
       Serial.println("medium");
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       break;
     case 3:  // your hand is nowhere near the sensor
       Serial.println("bright");
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
       break;
   }
   delay(1);  // delay in between reads for stability
