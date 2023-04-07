93a571b759fe49f23429169acc733cb9a9e68981
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-07 11:10:57 +0900

    add BUZZER
    밝기에 따라 BUZZER 소리 변경

--- switchCase_apply.ino
+++ switchCase_apply.ino
@@ -4,6 +4,7 @@ const int sensorMax = 600;  // sensor maximum, discovered through experiment
 const int LED1 = 6;
 const int LED2 = 9;
 const int LED3 = 10;
+const int BUZZER = 11;
 
 void setup() {
   // initialize serial communication:
@@ -11,6 +12,7 @@ void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
@@ -26,24 +28,28 @@ void loop() {
       digitalWrite(LED1, HIGH);
       digitalWrite(LED2, HIGH);
       digitalWrite(LED3, HIGH);
+      tone(BUZZER, 800);
       break;
     case 1:  // your hand is close to the sensor
       Serial.println("dim");
       digitalWrite(LED1, HIGH);
       digitalWrite(LED2, HIGH);
       digitalWrite(LED3, LOW);
+      tone(BUZZER, 400);
       break;
     case 2:  // your hand is a few inches from the sensor
       Serial.println("medium");
       digitalWrite(LED1, HIGH);
       digitalWrite(LED2, LOW);
       digitalWrite(LED3, LOW);
+      tone(BUZZER, 200);
       break;
     case 3:  // your hand is nowhere near the sensor
       Serial.println("bright");
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
       digitalWrite(LED3, LOW);
+      noTone(BUZZER);
       break;
   }
   delay(1);  // delay in between reads for stability
